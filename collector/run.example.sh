92a9edef5044328f7e694ce207478d0410e91993
typewriter <takuyaxp@gmail.com>
2019-11-17 17:21:52 +0900

    Revert tweet collector

--- collector/run.example.sh
+++ collector/run.example.sh
@@ -2,8 +2,7 @@
 
 PATH=$HOME/.rbenv/shims:$PATH
 cd `dirname "${0}"`
-bundle exec ./collector.rb ../config.yml > tweet.csv.tmp
-cat tweet.csv.tmp | sort | uniq > tweet.csv.tmp.2
-rm tweet.csv.tmp
-mv tweet.csv.tmp.2 tweet.csv
+bundle exec ./collector.rb ../config.yml >> tweet.csv
+cat tweet.csv | sort | uniq > tweet.csv.tmp
+mv tweet.csv.tmp tweet.csv
 
