1317625ec1390b0abaaf186ef9d9f75997391222
typewriter <takuyaxp@gmail.com>
2019-12-09 22:47:34 +0900

    Reduce memory consumption

--- analyzer/run.place_collector.sh
+++ analyzer/run.place_collector.sh
@@ -2,7 +2,9 @@
 
 PATH=$HOME/.rbenv/shims:$PATH
 cd `dirname "${0}"`
-cat ../collector/tweet.csv | bundle exec ./place_collector.rb ../config.yml results.json >> place_tweet.csv
-cat place_tweet.csv | sort | uniq > place_tweet.csv.tmp
-mv place_tweet.csv.tmp place_tweet.csv
+DATE=`date +%Y%m%d`
+
+bundle exec ./place_collector.rb ../config.yml results.json >> place_tweet_${DATE}.csv
+sort -S 100M -T /var/tmp place_tweet_${DATE}.csv | uniq > place_tweet_${DATE}.csv.tmp
+mv place_tweet_${DATE}.csv.tmp place_tweet_${DATE}.csv
 
--- collector/run.example.sh
+++ collector/run.example.sh
@@ -3,6 +3,6 @@
 PATH=$HOME/.rbenv/shims:$PATH
 cd `dirname "${0}"`
 bundle exec ./collector.rb ../config.yml >> tweet.csv
-cat tweet.csv | sort | uniq > tweet.csv.tmp
+sort -S 100M -T /var/tmp tweet.csv | uniq > tweet.csv.tmp
 mv tweet.csv.tmp tweet.csv
 
